5e1f8303f90906960d080dd099c8f906fc44ed7f
ZackNemec <zacknemec@live.com>
2020-07-31 20:00:21 -0700

    done

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import Characters from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  const [data, setData] = React.useState([]);
+  const [search, setSearch] = React.useState("");
+  const searchChange = (e) => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  React.useEffect(() => {
+    axios.get("https://rickandmortyapi.com/api/character").then((pull) => {
+      //console.log(pull.data);
+      const array1 = pull.data.results;
+      const arrayFilter = array1.filter((character) => {
+        return character.name.toLowerCase().includes(search.toLowerCase());
+      });
+      setData(arrayFilter);
+    });
+  }, [search]);
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <label>
+        Search:
+        <input
+          type="text"
+          name="search"
+          valve={search}
+          onChange={searchChange}
+        />
+      </label>
+      <Characters characterArray={data} />
     </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,34 @@
 // Write your Character component here
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardTitle,
+  CardText,
+  Container,
+  Col,
+  Row,
+} from "reactstrap";
+
+function Characters(props) {
+  return (
+    <Container>
+      <Row>
+        {props.characterArray.map((e) => {
+          return (
+            <Col xs="6" sm="4" md="3" lg="2">
+              <Card key={e.created}>
+                <CardImg src={e.image}></CardImg>
+                <CardTitle>{e.name}</CardTitle>
+                <CardText>Gender: {e.gender}</CardText>
+                <CardText>Status: {e.status}</CardText>
+                <CardText>Species: {e.species}</CardText>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+}
+export default Characters;
